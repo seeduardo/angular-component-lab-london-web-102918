71db368a5374c7a5da01319424547754b2a175ca
Todd Motto <todd@toddmotto.com>
2016-03-17 12:05:41 +0000

    chore: Changed variable name

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -4,11 +4,11 @@ describe('Directive Test', function() {
 	browser.get('http://localhost:8080');
 
 	it('should display the info correctly', function() {
-		var directive = element.all(by.css('contact-card'));
+		var component = element.all(by.css('contact-card'));
 
-		expect(directive.get(0).getText()).toContain('NAME');
-		expect(directive.get(0).getText()).toContain('EMAIL');
-		expect(directive.get(0).getText()).toContain('PHONE');
-		expect(directive.get(0).getText()).toContain('Bill Gates');
+		expect(component.get(0).getText()).toContain('NAME');
+		expect(component.get(0).getText()).toContain('EMAIL');
+		expect(component.get(0).getText()).toContain('PHONE');
+		expect(component.get(0).getText()).toContain('Bill Gates');
 	})
 });
